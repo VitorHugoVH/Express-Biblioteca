ea873889e51010f304ca0fb5e6cb6fe3c9854a8b
VitorHugoVH <vitorhugosf007@gmail.com>
2023-07-25 08:51:52 -0300

    You are so cool

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/cool', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Deu certo Joédio !');
 });
 
 module.exports = router;
