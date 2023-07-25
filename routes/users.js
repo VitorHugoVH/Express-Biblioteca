a51b3f256c943dbc2bf2b25ccb0a4134a3d590cf
VitorHugoVH <vitorhugosf007@gmail.com>
2023-07-25 09:12:02 -0300

    Commitando na Praia

--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,12 @@ router.get('/cool', function(req, res, next) {
   res.send('You are so cool. Deu certo Joédio !');
 });
 
+router.get('/cool/beach', function(req, res, next) {
+  res.send('Queria estar na praia, mas lembrei que moro na Gaivota.');
+});
+
+router.get('/ifc', function(req, res, next) {
+  res.send('Welcome to IFC - A melhor instituição!');
+});
+
 module.exports = router;
