939c98cf7a1fc9855e5d01011b2346c90be11a22
VitorHugoVH <vitorhugosf007@gmail.com>
2023-09-29 11:18:26 -0300

    Adicionado os arquivos para deployment

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
 const dev_db_url =
-  "mongodb+srv://arthurbc:FKfPGf97DxBnWL6q@projeto1.soogxlh.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://vhfraga007:ihRJAdsvUqUoABfe@cluster0.8ngc8te.mongodb.net/?retryWrites=true&w=majority";
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 main().catch((err) => console.log(err));
